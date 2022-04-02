368d644c8c54357bb2f7b6b4ad9279ce06e387a1
maesterlyb <yves.ekanga27@gmail.com>
2022-04-02 12:28:22 +0100

    [feat]: ajus logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,10 +26,10 @@ Author: GrayGrids
         var logo = document.querySelector('.navbar-brand img')
         if (window.pageYOffset > sticky) {
           header_navbar.classList.add("sticky");
-          logo.src = 'assets/images/logo/logoAC\ dark.jpg';
+          logo.src = 'assets/images/logo/logoAC.png';
         } else {
           header_navbar.classList.remove("sticky");
-          logo.src = 'assets/images/logo/logoAC.jpg';
+          logo.src = 'assets/images/logo/logoAC\ dark.png';
         }
 
         // show or hide the back-top-top button
